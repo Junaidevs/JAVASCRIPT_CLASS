f959733834c86d15ff97b6e6d15a4aee8c156040
junaidevs <junaidadeyinka20@gmail.com>
2025-07-22 16:22:35 +0100

    i learnt loop today

--- js/loop.js
+++ js/loop.js
@@ -29,3 +29,86 @@ console.log(num)
 num--
 console.log(num)
 
+// Decrease a numeric variable's value by 1
+
+let age = 42
+console.log(age)
+age+1
+console.log(age)
+age++
+console.log(age)
+
+//Increase a numeric var value
+
+let score = 50
+console.log(score)
+score--
+console.log(score)
+
+//Decrease a numeric var value by 1
+ 
+let previousyear = 2024
+console.log(previousyear)
+previousyear--
+console.log(previousyear)
+
+//subtract a value from a var and assign the result
+let myage = 12
+console.log(myage)
+let underage = myage -3 
+console.log(underage)
+
+//add a value to a var and assign it
+ let index = 4
+ console.log(index)
+ let more = index + 2
+ console.log(more)
+
+ //subtract a value from a var and assign the result, let money = 100
+
+let change = 150
+console.log(change)
+let money = change - 50
+console.log(money)
+
+ //multiply a  var by a value, and assign the result, let price = 20
+
+let discount = 10
+console.log(discount)
+let price = discount * 2
+console.log(price)
+
+
+ //loop
+ const fruits = ["apple", "banana", "cherry", "orange", "grape"]
+ 
+ for (let list = 0; list < fruits.length; list++){
+    console.log(fruits[list])
+ }
+
+const SN = [1,2,3,4,5,6,7,8,9,10]
+for (let all= 0; all < SN.length; all++){
+    console.log(SN[all])
+}
+
+// const number2 = [1,2,3,4,5,6,7,8,9,10]
+// for (let list= 0; list )
+
+
+const nomba = [2,3,4,9,12,15,23,30,31,33]
+console.log(nomba)
+for (let list = 0; list < nomba.length; list++)
+    if (nomba[list] % 2 === 0){
+        console.log(nomba[list]);
+
+}
+
+
+
+const number3 = [2,3,4,9,12,15,23,30,31,33]
+console.log(number3)
+for (let list = 0; list < number3.length; list++)
+    if ( number3[list] % 2 > 0) {
+        console.log(number3[list])
+
+}
